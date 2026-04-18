e677e84946fc3dbced56ccbac7284efc25fa4428
Peter Cullen <peter.cullen68@pm.me>
2026-04-17 21:08:04 -0400

    Exercise 9 Extra Test
    Small change to main test because of webkit timeouts.

--- e2e-tests/move-newpage.spec.js
+++ e2e-tests/move-newpage.spec.js
@@ -1,10 +1,10 @@
 import { test, expect }  from '@playwright/test'
 import {describe} from "node:test";
 
-describe('Pokedex', () => {
-  test('front page can be opened', async ({ page }) => {
-    await page.goto('')
-    await expect(page.getByText('ivysaur')).toBeVisible({ timeout: 10000 });
+describe('charizard', () => {
+  test('open charizard page', async ({ page }) => {
+    await page.goto('/pokemon/charizard')
+    await expect(page.getByText('charizard')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
   })
 })
