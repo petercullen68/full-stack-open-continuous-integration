2df7c3fee9e2fe4d92d05df3ada3ca3533e450f8
Peter Cullen <peter.cullen68@pm.me>
2026-04-19 13:45:58 -0400

    Exercise 12
    Simulate a bad health check

--- app.js
+++ app.js
@@ -11,8 +11,11 @@ app.get('/version', (req, res) => {
 })
 
 app.get('/health', (req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw('error...  ')
   res.send('ok')
 })
+
 const start = async () => {
   await app.listen(PORT)
   console.log(`server started on port ${PORT}`)
