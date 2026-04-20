e7ce7dd6ac65a72d55fbf6d437524bbc84615930
Peter Cullen <peter.cullen68@pm.me>
2026-04-20 15:17:41 -0400

    Exercise 17 fix linting error

--- jest.setup.js
+++ jest.setup.js
@@ -1,4 +1,5 @@
 import { TextEncoder, TextDecoder } from 'util';
 // eslint-disable-next-line no-undef
 global.TextEncoder = TextEncoder;
+// eslint-disable-next-line no-undef
 global.TextDecoder = TextDecoder;
