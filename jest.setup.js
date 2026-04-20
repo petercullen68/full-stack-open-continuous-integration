170e3a930ab7c308de8d1ed4a59119d789126bc9
Peter Cullen <peter.cullen68@pm.me>
2026-04-20 15:15:01 -0400

    Exercise 17
    Fail, linting step will fail build

--- jest.setup.js
+++ jest.setup.js
@@ -1,5 +1,4 @@
 import { TextEncoder, TextDecoder } from 'util';
 // eslint-disable-next-line no-undef
 global.TextEncoder = TextEncoder;
-// eslint-disable-next-line no-undef
 global.TextDecoder = TextDecoder;
